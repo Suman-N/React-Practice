d733a3e9913c2fb5f8459bbb4f39f62d2dc6e83e
Suman-N <nandalsuman7@gmail.com>
2023-12-15 19:28:55 +0530

    creating react application without using vite

---
+++ react-project/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <><h1>Hello World | Suman Nandal</h1>
+   <p>Today i have lernt  creating a React Application using <b>vite</b> with create-react-app we are having some unused dependencies to avoid things thing nowadays we are using <b>vite</b></p></>
+   
+  );
+}
+
+export default App;
