da186f70a8235a7934a62604163f901ebb3ae48a
Suman-N <nandalsuman7@gmail.com>
2024-01-02 18:52:04 +0530

    content changes for theme switcher

--- theme-Switcher/src/Component/Card.jsx
+++ theme-Switcher/src/Component/Card.jsx
@@ -1,16 +1,31 @@
+import useTheme from "../Context/Theme";
+
 export default function Card() {
+
+    const { thememode } = useTheme()
     return (
-        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white border border-gray-200 rounded-lg shadow">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                {thememode === "dark" ?
+                    <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/1716158/pexels-photo-1716158.jpeg?auto=compress&cs=tinysrgb&w=600" alt="product_image1" /> :
+                    <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=600" />
+                }
+
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-                    </h5>
+                    {thememode === "dark" ?
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-black">
+                            The Moon is magic for the soul and light for the senses
+
+                        </h5> :
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-black">
+                            A sunrise is Nature telling you that it&apos;s time to cherish another day on Earth. Enjoy life
+
+                        </h5>}
+
                 </a>
-                <div className="flex items-center mt-2.5 mb-5">
+                {/* <div className="flex items-center mt-2.5 mb-5">
                     <svg
                         className="w-4 h-4 text-yellow-300 mr-1"
                         aria-hidden="true"
@@ -48,7 +63,7 @@ export default function Card() {
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                     </svg>
                     <svg
-                        className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                        className="w-4 h-4 text-gray-600 dark:text-gray-600"
                         aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="currentColor"
@@ -61,14 +76,14 @@ export default function Card() {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-black">$599</span>
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                         Add to cart
                     </a>
-                </div>
+                </div> */}
             </div>
         </div>
     );
